fdd41f81f5bf51da17887f88bdd68d89e3b07ecc
Gokul <75981297+g0kul6@users.noreply.github.com>
2021-08-20 14:55:08 +0530

    parameter.py

--- parameter.py
+++ parameter.py
@@ -2,6 +2,14 @@
 import gym
 #creating the environment CartPole
 env = gym.make('CartPole-v0')
+
+#size of state and action
 state_size=env.observation_space.shape[0]
 action_size=env.action_space.n
+
+#batch size
+batch_size=32
+
+#numer of episodes for training
+n_episodes=1001
 
