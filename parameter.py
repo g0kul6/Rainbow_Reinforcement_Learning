23c425be65a0529ac8c0b13a1b56f842ddcbfc73
Gokul <75981297+g0kul6@users.noreply.github.com>
2021-08-20 14:47:56 +0530

    parameters

---
+++ parameter.py
@@ -0,0 +1,7 @@
+#creating a environment to test our agent
+import gym
+#creating the environment CartPole
+env = gym.make('CartPole-v0')
+state_size=env.observation_space.shape[0]
+action_size=env.action_space.n
+
